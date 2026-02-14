01d7f92dd8cd2f7e1be505b1a62d3db7ab69835e
Thomas Weichselbaumer <t.weichselbaumer@inpsyde.com>
2026-02-14 16:58:12 +0100

    Add excerp to blog posts

--- src/content.config.ts
+++ src/content.config.ts
@@ -7,6 +7,7 @@ const posts = defineCollection({
 		z.object({
 			title: z.string(),
 			description: z.string(),
+			excerpt: z.string().optional(),
 			pubDate: z.coerce.date(),
 			updatedDate: z.coerce.date().optional(),
 		}),
