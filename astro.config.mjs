36f1db0dc2b90a48422a743caf892618a5da2519
Thomas Weichselbaumer <t.weichselbaumer@inpsyde.com>
2026-02-20 21:56:45 +0100

    Add Astro sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
-	site: 'https://thomasweichselbaumer.com',
-  trailingSlash: 'always',
-  vite: {
-    plugins: [tailwindcss()]
-  }
+    site: 'https://thomasweichselbaumer.com',
+    trailingSlash: 'always',
+    integrations: [sitemap()],
+    vite: {
+      plugins: [tailwindcss()]
+    },
 });
