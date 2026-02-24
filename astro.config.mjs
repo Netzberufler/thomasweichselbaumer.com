b9ab2cdbe81752ccb15c1b8f2d38b24ba78b1db3
Thomas Weichselbaumer <t.weichselbaumer@inpsyde.com>
2026-02-24 20:43:54 +0100

    Simplify ESLint setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,19 +2,12 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
-import eslint from 'vite-plugin-eslint';
 
 export default defineConfig({
 	site: 'https://thomasweichselbaumer.com',
 	trailingSlash: 'always',
 	integrations: [sitemap()],
 	vite: {
-		plugins: [
-			tailwindcss(),
-			eslint({
-				include: ['src/**/*.{js,ts,astro}'],
-				exclude: ['node_modules'],
-			}),
-		],
+		plugins: [tailwindcss()],
 	},
 });
