977b4f7672b2cca451e4eb5e8d608fca7dc50089
Thomas Weichselbaumer <t.weichselbaumer@inpsyde.com>
2026-02-24 20:12:23 +0100

    Add ESLint and Prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,20 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
+import eslint from "vite-plugin-eslint";
 
 export default defineConfig({
-    site: 'https://thomasweichselbaumer.com',
-    trailingSlash: 'always',
-    integrations: [sitemap()],
-    vite: {
-      plugins: [tailwindcss()]
-    },
+	site: "https://thomasweichselbaumer.com",
+	trailingSlash: "always",
+	integrations: [sitemap()],
+	vite: {
+		plugins: [
+			tailwindcss(),
+			eslint({
+				include: ["src/**/*.{js,ts,astro}"],
+				exclude: ["node_modules"],
+			}),
+		],
+	},
 });
